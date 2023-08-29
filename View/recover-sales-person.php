4315037d8c79f331dbe8a1e29790e2ad23caf0fc
tanvir103 <tanvirh103@gmail.com>
2023-08-30 05:15:54 +0600

    .

--- View/recover-sales-person.php
+++ View/recover-sales-person.php
@@ -1,3 +1,4 @@
+<?php require_once('../Model/userinfo_model.php'); $result=viewbannedSalesPerson();?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,5 +11,27 @@
         <font face="times new roman" size="7">Recover Sales Person</font>
         <hr width="40%" color="#530f61">
     </center>
+    <table align="center" cellpadding="25" cellspacing="0">
+    <?php
+    if(mysqli_num_rows($result)>0){
+       echo" <tr><td><font face=\"times new roman\" size=\"5\">Customer ID</font></td>
+        <td><font face=\"times new roman\" size=\"5\">Customer Name</font></td>
+        <td><font face=\"times new roman\" size=\"5\">Customer Email</font></td>
+        <td><font face=\"times new roman\" size=\"5\">Action</font></td>
+        </tr>";
+    while($row=mysqli_fetch_assoc($result)){
+        $userid=$row['UserID'];
+        $username=$row['Username'];
+        $email=$row['Email'];
+        echo"<tr>
+        <td><font face=\"times new roman\" size=\"5\">$userid</font></td>
+        <td><font face=\"times new roman\" size=\"5\">$username</font></td>
+        <td><font face=\"times new roman\" size=\"5\">$email</font></td>
+        <td><font face=\"times new roman\" size=\"5\"><a href=\"../Controller/recover-controller.php?id={$userid}\"><button>Recover Customer</button></font></a></td>
+        </tr>";
+    }}else{
+        echo"<tr><td><font face=\"times new roman\" size=\"5\">No Sales Person Found For Recover</font></td></tr>";
+    }
+    ?>
 </body>
 </html>
