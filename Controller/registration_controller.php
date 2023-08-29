850a80ce0e64d18a845c1e25e96a3a37b025b1b0
tanvir103 <tanvirh103@gmail.com>
2023-08-29 23:32:16 +0600

    .

--- Controller/registration_controller.php
+++ Controller/registration_controller.php
@@ -1,3 +1,17 @@
 <?php
+require_once('../Model/userinfo_model.php');
+    if(isset($_POST['submit'])){
+        $username=$_POST['name'];
+        $email=$_POST['email'];
+        $phone=$_POST['phone'];
+        $password=$_POST['password'];
 
+        $result=addCustomer($username,$email,$phone,$password);
+        if($reuslt){
+            echo "Account Created";
+        }else{
+            echo "Failed";
+        }
+        
+    }
 ?>
--- Model/userinfo_model.php
+++ Model/userinfo_model.php
@@ -1,3 +1,9 @@
 <?php
-
+require_once('db_connection.php');
+function addCustomer($username,$email,$phone,$password){
+    $conn=dbConnection();
+    $sql="INSERT INTO userinfo VALUES ('','$password','$email','$username','$phone','Customer','Active')";
+    $result=mysqli_query($conn,$sql);
+    return $result;
+}
 ?>
