b5137be2a4c534168e784faf999fdb9e39ea634a
tanvir103 <tanvirh103@gmail.com>
2023-08-30 03:12:08 +0600

    .

---
+++ View/payment-approval.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Payment Approval</title>
+</head>
+<body>
+    <center>
+        <font face="times new roman" size="7">Payment Approval</font>
+        <hr width="40%" color="#530f61">
+    </center>
+</body>
+</html>
