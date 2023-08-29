9ecf2d99c3ff888261368fffca045f9b8627af9f
tanvir103 <tanvirh103@gmail.com>
2023-08-30 01:01:22 +0600

    .

---
+++ View/searchproduct.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="../script.js"></script>
+    <title>Search Product</title>
+</head>
+<body>
+    <center>
+        <font face="times new roman" size="7">Search Product</font>
+        <hr width="35%" color="#530f61">
+    </center>
+    <table align="center">
+        <br>
+        <tr><td><input type="text" size="40px" id="search" onkeypress="liveSearch(this.value)"></td></tr>
+        <tr><td><font face="times new roman" id="message" ><br>Please type product nam</font></td></tr>
+    </table>
+</body>
+</html>
