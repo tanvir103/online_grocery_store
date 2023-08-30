94bba2a7b607e5a337c131a117c728d98e750a10
tanvir103 <tanvirh103@gmail.com>
2023-08-30 23:33:19 +0600

    .

--- Controller/logout-controller.php
+++
@@ -1,5 +0,0 @@
-<?php
-setcookie("flag1","",time()-999999,"/");
-setcookie("flag2","",time()-360000,"/");
-header('location:../View/signin.html');
-?>
---
+++ View/logout-controller.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+    <img src="../Image/logout.gif" alt="Logout Immage" width="500px" height="400px">
+    </center>
+</body>
+</html>
+<?php
+setcookie("flag1","",time()-999999,"/");
+setcookie("flag2","",time()-360000,"/");
+header("refresh:2;url=../View/signin.html");
+?>
+
