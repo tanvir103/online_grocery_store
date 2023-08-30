f2e099d266ca45823c2abbe97974d37b1f066723
tanvir103 <tanvirh103@gmail.com>
2023-08-31 00:21:57 +0600

    .

--- View/addproduct.php
+++ View/addproduct.php
@@ -10,5 +10,27 @@
         <font face="times new roman" size="7">Add product</font>
         <hr width="40%" color="#530f61">
     </center>
+    <table align="center" cellpadding="25" cellspacing="0" width="auto" border="1" bordercolor="#91b867">
+        <form action="../Controller/add-product-controller.php" method="post">
+        <tr><td>
+            <font face="times new roman" size="4">Product Name:</font>
+            <input type="text" name="name"><br><br>
+            <font face="times new roman" size="4">Product Category:</font>
+            <select name="category">
+                <option selected>Select Category</option>
+                <option value="Personal Care">Personal Care</option>
+                <option value="Frozen Foods">Frozen Foods</option>
+                <option value="Snacks">Snacks</option>
+                <option value="Dairy">Dairy</option>
+                <option value="Baby Items">Baby Items</option>
+            </select><br><br>
+            <font face="times new roman" size="4">Product Price:</font>
+            <input type="number" name="price"><br><br>
+            <font face="times new roman" size="4">Product Quantity:</font>
+            <input type="number" name="quantity"><br><br>
+            <button name="submit">Add Product</button>
+        </form>
+        </td></tr>
+    </table>
 </body>
 </html>
