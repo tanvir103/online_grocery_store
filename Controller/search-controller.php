aea84e1804cd77761e01bb72d6480873c1526ba4
tanvir103 <tanvirh103@gmail.com>
2023-08-31 03:31:46 +0600

    .

--- Controller/search-controller.php
+++ Controller/search-controller.php
@@ -1,4 +1,28 @@
 <?php
+    require_once('../Model/product_model.php');
     $product=$_REQUEST['name'];
-    echo $product;
+    $result=searchProduct($product);
+    if(mysqli_num_rows($result) > 0) {
+        echo"<br><br><table width=\"120%\" border=\"1\" cellpadding=\"0\" cellspacing=\"0\">
+        <tr><td align=\"center\">Product Name</td>
+            <td align=\"center\">Product Price</td>
+            <td align=\"center\">Product Quantity</td>
+            <td align=\"center\">Product Picture</td></tr>";
+        while($row = mysqli_fetch_assoc($result)) {
+            $productname=$row['ProductName'];
+            $productprice=$row['ProductPrice'];
+            $productquantity=$row['ProductQuantity'];
+            $picture=$row['ProductPicture'];
+
+            echo"
+            <tr><td align=\"center\">$productname</td>
+            <td align=\"center\">$productprice</td>
+            <td align=\"center\">$productquantity</td>
+            <td ><img src=\"$picture\"width=\"250px\"></td>
+        </tr>";
+        }
+        echo" </table>";
+    }else{
+        echo "<br><br>No Product Found";
+    }
 ?>
--- Model/product_model.php
+++ Model/product_model.php
@@ -1,8 +1,16 @@
 <?php
     require_once('db_connection.php');
+    
     function insertProduct($name,$category,$price,$quantity,$picture){
         $conn=dbConnection();
         $sql="INSERT INTO productinfo VALUES ('','$name','$category','$price','$quantity','$picture','')";
+        $result=mysqli_query($conn,$sql);
+        return $result;
+    }
+
+    function searchProduct($name){
+        $conn=dbConnection();
+        $sql="SELECT * FROM productinfo WHERE ProductName LIKE'%$name%'";
         $result=mysqli_query($conn,$sql);
         return $result;
     }
