791e78032bc3e6b52104878c92682218d557d55c
tanvir103 <tanvirh103@gmail.com>
2023-09-01 03:00:42 +0600

    .

---
+++ View/view-profile-info.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile Information</title>
+</head>
+<body>
+    <center>
+        <font face="times new roman" size="7">Profile Information</font>
+        <hr width="40%" color="#530f61">
+    </center>
+    
+</body>
+</html>
